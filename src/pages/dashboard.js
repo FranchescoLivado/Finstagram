5fed7e0530d26b91929ee957720debf1d74d761f
FranchescoLivado <39171544+FranchescoLivado@users.noreply.github.com>
2021-06-13 03:21:55 -0400

    web font has different display compared to desktop font. Scrapped web font and used prototype svgs

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -7,15 +7,17 @@ import LoginAcc from '../components/dashboard-btns/loginAcc';
 export default function Dashboard() {
   return (
     <div className="container flex mx-auto max-w-screem-md items-center h-screen">
-      <div className="flex">
+      <div className="flex w-2/5">
         <img src="/images/Fox_-_Super_Smash_Bros 1.png" alt="melee character" />
       </div>
-      <div className="text-center">
-        <h1 className="text-white font-bold text-7xl">Connect To Your Competition</h1>
-        <h2 className="text-white font-bold text-4xl">
-          using the new netplay
-          <img className="inline pl-4" src="/images/SlippiLogo.svg" alt="Slippi Logo" />
-        </h2>
+      <div className="text-center w-1/2">
+        <img
+          className="inline pl-4 mb-10"
+          src="images/Connect to Your Competition.svg"
+          alt="Connect to your competition"
+        />
+
+        <img className="inline pl-4 mb-28" src="images/heading 2.svg" alt="with the new netplay" />
 
         <div>
           <p className="text-white">Dont have an account?</p>
